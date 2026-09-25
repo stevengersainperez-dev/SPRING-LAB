ef3c2f9ea44b34e310b3bc879356ed21bd7ad79f
Steven Dev <stevengersainperez@gmail.com>
2026-09-25 05:59:07 -0600

    Sin test

--- src/main/java/com/spring_lab/SpringLabApplication.java
+++ src/main/java/com/spring_lab/SpringLabApplication.java
@@ -1,13 +1,21 @@
 package com.spring_lab;
 
+import com.spring_lab.mat.Operation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringLabApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringLabApplication.class, args);
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLabApplication.class, args);
+        Operation operation = new Operation();
+
+
+        System.out.println("reultado de la operacion: "+operation.suma(4,5));
+
 	}
 
 }
---
+++ src/main/java/com/spring_lab/mat/Operation.java
@@ -0,0 +1,14 @@
+package com.spring_lab.mat;
+
+public class Operation {
+
+    public int suma(int num1, int num2){
+
+        return num1+num2;
+    }
+
+    public int resta(int num1, int num2){
+
+        return num1-num2;
+    }
+}
